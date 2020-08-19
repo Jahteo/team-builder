41abf18fd7eacf8a4d590c1c79163ef7b366169f
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-19 13:43:39 -0700

    set up state to keep team member list w several key:value pairs.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  const initialTeamList = {
+    name: "sofie",
+    email: "sofie@sofie.com",
+    role: "mascot doggo"
+  }
+const [teamList, setTeamList] = useState([initialTeamList])
+
+console.log(teamList)
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }

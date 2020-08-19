c4c2871de1d63ed7318ad53eb28c580856f1def0
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-19 14:00:02 -0700

    created Form, successfully rendering in app.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from "./components/Form"
 
 function App() {
   const initialTeamList = {
@@ -23,6 +24,7 @@ function App() {
           </div>
         )
       })}
+      <Form />
     </div>
   );
 }
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Form () {
+
+
+    return (
+        <div>
+            Tesing form
+        </div>
+    )
+}
+
+export default Form
